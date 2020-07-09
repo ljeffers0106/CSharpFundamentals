99c526c4d9ab627d9e4e8fe1149df12ac3472c25
Lisa Jeffers <lisajeffers0106@gmail.com>
2020-07-09 14:13:41 -0400

    Before presentations

--- RobTheBank_Console/ProgramUI.cs
+++ RobTheBank_Console/ProgramUI.cs
@@ -14,32 +14,32 @@ namespace RobTheBank_Console
 
         {
             Questions questionOne = new Questions();
-            questionOne.QuestionDescription = "What time would be best to rob a bank?";
+            questionOne.QuestionDescription = " What time would be best to rob a bank?";
             questionOne.Question = 1;
-            questionOne.AnswerOne = "1.) Morning";
-            questionOne.AnswerTwo = "2.) Afternoon";
-            questionOne.AnswerThree = "3.) Night";
+            questionOne.AnswerOne = " 1.) Morning";
+            questionOne.AnswerTwo = " 2.) Afternoon";
+            questionOne.AnswerThree = " 3.) Night";
 
             Questions questionTwo = new Questions();
-            questionTwo.QuestionDescription = "What weapon would you use to rob the bank?";
+            questionTwo.QuestionDescription = " What weapon would you use to rob the bank?";
             questionTwo.Question = 2;
-            questionTwo.AnswerOne = "1.) Knife";
-            questionTwo.AnswerTwo = "2.) Bomb";
-            questionTwo.AnswerThree = "3.) Gun";
+            questionTwo.AnswerOne = " 1.) Knife";
+            questionTwo.AnswerTwo = " 2.) Bomb";
+            questionTwo.AnswerThree = " 3.) Gun";
 
             Questions questionThree = new Questions();
-            questionThree.QuestionDescription = "How many members would you have on your crew?";
+            questionThree.QuestionDescription = " How many members would you have on your crew?";
             questionThree.Question = 3;
-            questionThree.AnswerOne = "1.) 1";
-            questionThree.AnswerTwo = "2.) 2";
-            questionThree.AnswerThree = "3.) 4";
+            questionThree.AnswerOne = " 1.) 1";
+            questionThree.AnswerTwo = " 2.) 2";
+            questionThree.AnswerThree = " 3.) 4";
 
             Questions questionFour = new Questions();
-            questionFour.QuestionDescription = "Choose your method of escape!";
+            questionFour.QuestionDescription = " Choose your method of escape!";
             questionFour.Question = 4;
-            questionFour.AnswerOne = "1.) Hot Air Baloon";
-            questionFour.AnswerTwo = "2.) Private Jet";
-            questionFour.AnswerThree = "3.) Moped";
+            questionFour.AnswerOne = " 1.) Hot Air Baloon";
+            questionFour.AnswerTwo = " 2.) Private Jet";
+            questionFour.AnswerThree = " 3.) Moped";
 
 
 
@@ -48,18 +48,18 @@ namespace RobTheBank_Console
             _repo.AddQuestionsToList(questionThree);
             _repo.AddQuestionsToList(questionFour);
 
-            Responses one = new Responses("Too Early", "1.) Morning");
-            Responses two = new Responses("Too Many People", "2.) Afternoon");
-            Responses three = new Responses("Just Right - Proceed to next question", "3.) Night");
-            Responses four = new Responses("Pocket Knife isn't Big Enough!", "1.) Knife");
-            Responses five = new Responses("Perfect Solution - Very Scary - Proceed to next question", "2.) Bomb");
-            Responses six = new Responses("No Bullets!", "3.) Gun");
-            Responses seven = new Responses("All by Yourself!", "1.) 1");
-            Responses eight = new Responses("You and Your Best Friend?", "2.) 2");
-            Responses nine = new Responses("Enough Peope to Provide 2 on Lookout and 2 to Carry the Money - Proceed to next question", "3.) 4");
-            Responses ten = new Responses("Hot Air Ballon, UP UP and Away - Success!!!!", "1.) Hot Air Baloon");
-            Responses eleven = new Responses("You Spent the Money too Soon!", "2.) Private Jet");
-            Responses twelve = new Responses("Too Many People for One Moped!", "3.) Moped");
+            Responses one = new Responses("  Too Early", " 1.) Morning");
+            Responses two = new Responses("  Too Many People", " 2.) Afternoon");
+            Responses three = new Responses("  Just Right - Proceed to next question", " 3.) Night");
+            Responses four = new Responses("  Pocket Knife isn't Big Enough!", " 1.) Knife");
+            Responses five = new Responses("  Perfect Solution - Very Scary - Proceed to next question", " 2.) Bomb");
+            Responses six = new Responses("  No Bullets!", " 3.) Gun");
+            Responses seven = new Responses("  All by Yourself!", " 1.) 1");
+            Responses eight = new Responses("  You and Your Best Friend?", " 2.) 2");
+            Responses nine = new Responses("  Enough Peope to Provide 2 on Lookout and 2 to Carry the Money - Proceed to next question", " 3.) 4");
+            Responses ten = new Responses("  Hot Air Ballon, UP UP and Away - Success!!!!", " 1.) Hot Air Baloon");
+            Responses eleven = new Responses("  You Spent the Money too Soon!", " 2.) Private Jet");
+            Responses twelve = new Responses("  Too Many People for One Moped!", " 3.) Moped");
 
             _repo.AddResponseToList(one);
             _repo.AddResponseToList(two);
@@ -113,14 +113,14 @@ namespace RobTheBank_Console
             Console.WriteLine("Press Any Key to Continue...");
             Console.ReadKey();
             
-
+            
             int numberVariable = 1;
             
             bool gameOn = true;
 
             while (gameOn)
             {
-                string shortResponse;
+                
                 Console.Clear();
                 string reply = NewQuestion(numberVariable);
                 switch (numberVariable)
@@ -129,23 +129,28 @@ namespace RobTheBank_Console
 
                          if (reply == "1")
                          { 
-                            // shortResponse = "1.) Morning";
-                            ReadResponse("1.) Morning");
+                           
+                            ReadResponse(" 1.) Morning");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                          if (reply == "2")
                          {
-                            //shortResponse = "2.) Afternoon";
-                            ReadResponse("2.) Afternoon");
+                            
+                            ReadResponse(" 2.) Afternoon");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                          if (reply == "3")
                          {
-                            //shortResponse = "3.) Night";
-                            ReadResponse("3.) Night");
+                            
+                            ReadResponse(" 3.) Night");
+                            Console.ReadKey();
                             numberVariable = 2;
                          }
                          if (reply != "1" && reply != "2" && reply != "3")
                          {
-                            
+                            Console.Clear();
                             Console.WriteLine(" Try Again - number must be 1,2,or 3!");
                             Console.ReadKey();
                         }
@@ -153,57 +158,70 @@ namespace RobTheBank_Console
                     case 2:
                         if (reply == "1")
                         {
-                            // shortResponse = "1.) Morning";
-                            ReadResponse("1.) Knife");
+                            
+                            ReadResponse(" 1.) Knife");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply == "2")
                         {
-                            //shortResponse = "2.) Afternoon";
-                            ReadResponse("2.) Bomb");
+                            
+                            ReadResponse(" 2.) Bomb");
+                            Console.ReadKey();
                             numberVariable = 3;
                         }
                         if (reply == "3")
                         {
-                            //shortResponse = "3.) Night";
-                            ReadResponse("3.) Gun");
+                            
+                            ReadResponse(" 3.) Gun");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply != "1" && reply != "2" && reply != "3")
                         {
+                            Console.Clear();
                             Console.WriteLine(" Try Again - number must be 1,2,or 3!");
                             Console.ReadKey();
                         }
-                        //ReadResponse(shortResponse);
+                        
                         break;
                     case 3:
                         if (reply == "1")
                         {
-                            // shortResponse = "";
-                            ReadResponse("1.) 1");
+                            
+                            ReadResponse(" 1.) 1");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply == "2")
                         {
-                            //shortResponse = "";
-                            ReadResponse("2.) 2");
+                           
+                            ReadResponse(" 2.) 2");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply == "3")
                         {
-                            //shortResponse = "";
-                            ReadResponse("3.) 4");
+                           
+                            ReadResponse(" 3.) 4");
+                            Console.ReadKey();
                             numberVariable = 4;
                         }
                         if (reply != "1" && reply != "2" && reply != "3")
                         {
+                            Console.Clear();
                             Console.WriteLine(" Try Again - number must be 1,2,or 3!");
                             Console.ReadKey();
                         }
-                        //ReadResponse(shortResponse);
+                        
                         
                         break;
                     case 4:
                         if (reply == "1")
                         {
-                            // shortResponse = "";
-                            ReadResponse("1.) Hot Air Baloon");
+                            
+                            ReadResponse(" 1.) Hot Air Baloon");
+                            Console.WriteLine("                                      ");
                             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
                             Console.WriteLine(" !!! You are READY to Rob the Bank !!!");
                             Console.ReadKey();
@@ -211,21 +229,26 @@ namespace RobTheBank_Console
                         }
                         if (reply == "2")
                         {
-                            //shortResponse = "";
-                            ReadResponse("2.) Private Jet");
+                           
+                            ReadResponse(" 2.) Private Jet");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply == "3")
                         {
-                            //shortResponse = "";
-                            ReadResponse("3.) Moped");
+                           
+                            ReadResponse(" 3.) Moped");
+                            Console.WriteLine(" Wrong Answer - Try Again");
+                            Console.ReadKey();
                         }
                         if (reply != "1" && reply != "2" && reply != "3")
                         {
+                            Console.Clear();
                             Console.WriteLine(" Try Again - number must be 1,2,or 3!");
                             Console.ReadKey();
 
                         }
-                        //ReadResponse(shortResponse);
+                        
                         break;
                     default:
                         gameOn = false;
@@ -238,6 +261,7 @@ namespace RobTheBank_Console
 
                 Questions questioninfo = _repo.GetQuestionByNumber(number);
                 Console.WriteLine(questioninfo.QuestionDescription);
+                Console.WriteLine();
                 Console.WriteLine(questioninfo.AnswerOne);
                 Console.WriteLine(questioninfo.AnswerTwo);
                 Console.WriteLine(questioninfo.AnswerThree);
@@ -245,12 +269,11 @@ namespace RobTheBank_Console
                 return (reply);
             }
             void ReadResponse(string description)
-            {
+            {;
+                Console.Clear();
                 Responses responseinfo = _repo.GetResponsesByNumber(description);
                 Console.WriteLine(responseinfo.Answer);
                 Console.WriteLine(responseinfo.Response);
-
-                Console.ReadKey();
             }
            
 
